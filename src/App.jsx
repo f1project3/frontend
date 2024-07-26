96b7ed1389a0db12c2c53e43296190655d6acbaf
azubal22 <amzubal@cincinnatistate.edu>
2024-07-26 19:17:21 -0400

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-// src/App.jsx
 import "./App.css";
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
+import DriverDetails from "./Components/DriverDetails";
+import Standings from "./Components/Standings";
+import DriverList from "./Components/DriverList";
+import TeamDetail from "./Components/TeamDetail";
+import TeamList from "./Components/TeamList";
+
+
 import Home from "./pages/Home";
 
 function App() {
@@ -10,6 +16,11 @@ function App() {
       <Header />
       <Navbar />
       <Home />
+      <DriverDetails />
+      <Standings />
+      <DriverList />
+      <TeamDetail />
+      <TeamList />
     </div>
   );
 }
